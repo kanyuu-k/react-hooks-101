f4a140abdad83ae54288cf0bcaee8acb544ce3e0
kanyuu-k <itumo_rakuten@yahoo.co.jp>
2021-02-26 21:12:41 +0900

    my frist React Hook with useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React,{ useState } from 'react';
 
 const App= () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  const increment2 = () => setCount(previousCount => previousCount + 1);
+  const decrement2 = () => setCount(previousCount => previousCount - 1);
+  const Reset = () => setCount(0);
+  const count2 = () => setCount(previousCount => previousCount * 2);
+
+  const divide3 = () => setCount( previousCount => previousCount % 3 === 0 ? previousCount / 3 :
+ previousCount
+ )
+
+  
+
   return (
+    <>
+    <div>count: {count}</div>
     <div>
-	This is a templete for React app.
+    <button onClick={increment}>+1</button>
+    <button onClick={decrement}>-1</button>
     </div>
+    <div>
+    <button onClick={increment2}>+1</button>
+    <button onClick={decrement2}>-1</button>
+    <div>
+      <button onClick={Reset}>Reset</button>
+      <button onClick={count2}>Count×2</button>
+      <button onClick={divide3}>Waru</button>
+
+    </div>
+    </div>
+
+    </>
   );
 }
 
